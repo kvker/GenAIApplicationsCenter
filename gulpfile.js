c84d4e924a90c07e40f6fb017f90712fba628ed2
leghair <zweizhao@foxmail.com>
2023-11-05 10:54:48 +0800

    feat 使用gulp压缩

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const { src, parallel, dest, watch } = require('gulp')
+const htmlmin = require('gulp-htmlmin')
+const uglify = require('gulp-uglify')
+const cleanCss = require('gulp-clean-css')
+const rename = require('gulp-rename')
+
+// const dev_url = 'src/'
+// src = path => src(dev_url + path)
+
+function htmls() {
+  return src('src/*.html')
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        minifyJS: true,
+        minifyCSS: true,
+      })
+    )
+    .pipe(dest('dist/'))
+}
+
+function styles() {
+  return src('src/styles/**/*.css')
+    .pipe(cleanCss())
+    // .pipe(
+    //   rename(function (path) {
+    //     if(path.basename.includes('.min')) {
+    //       return
+    //     }
+    //     // Returns a completely new object, make sure you return all keys needed!
+    //     return {
+    //       dirname: path.dirname,
+    //       basename: path.basename,
+    //       extname: '.min.css',
+    //     }
+    //   })
+    // )
+    .pipe(dest('dist/styles/'))
+}
+
+function scripts() {
+  return src('src/scripts/**/*.js').pipe(uglify()).pipe(dest('dist/scripts/'))
+}
+
+function libs() {
+  return src('src/libs/**/*').pipe(dest('dist/libs/'))
+}
+
+function icons() {
+  return src('src/icons/**/*').pipe(dest('dist/icons/'))
+}
+
+function images() {
+  return src('src/images/**/*').pipe(dest('dist/images/'))
+}
+
+function docs() {
+  return src('src/docs/**/*').pipe(dest('dist/docs/'))
+}
+
+exports.default = parallel(htmls, styles, scripts, libs, icons, images, docs)
+
+// watch(['src/*', 'gulpfile.js'], parallel(htmls, styles, scripts))
